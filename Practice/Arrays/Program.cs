22e68f93b4d7d98e4d84b55c5bbd3d63a3bc0291
Md Mahabubul  Alam Abir <mahbubabir09@gmail.com>
2024-07-31 23:55:25 +0600

    Arrays

---
+++ Practice/Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //normal or dimensional array
+            int[] x = new int[5];
+            x[0] = 1;
+
+            Console.WriteLine(x[0]);
+
+            int[,] y = new int[5, 5];
+            y[0, 0] = 7;
+
+            Console.WriteLine(y[0,0]);
+
+
+            //jagged arrays
+
+            int [][] z = new int[5][];
+
+            z[0] = new int[] {1,2};
+            z[1] = new int[] { 3, 4, 5 };
+            z[2] = new int[] { 6 };
+            z[3] = new int[] { 7 };
+            z[4] = new int[] { 8, 9, 10 };
+
+            for(int row= 0; row < z.Length; row++)
+            {
+                for(int col = 0; col < z[row].Length; col++)
+                {
+                    Console.Write(z[row][col]);
+                }
+                Console.WriteLine();
+            }
+
+            
+        }
+    }
+}
