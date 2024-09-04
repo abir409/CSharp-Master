9e365365b68dd97bac80500463214cbadfc5f9f0
Md Mahabubul  Alam Abir <mahbubabir09@gmail.com>
2024-09-04 17:55:32 +0600

    Interface part two

--- Practice/InterfaceUse/Program.cs
+++ Practice/InterfaceUse/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,15 @@ namespace InterfaceUse
             string data = laptop.TransferDataFromPort1();
 
             Console.WriteLine(data);
+
+            HoverCraft hovercraft = new HoverCraft();
+            hovercraft.StartEngine();
+
+            ClassA c = new ClassB(); //boxing
+
+            ClassB c3 = (ClassB)c; //unboxing
+
+            c3.Method3();
         }
     }
 }
